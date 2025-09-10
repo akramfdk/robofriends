011ce40718d242bd1b37bd5aa07958befaed5736
akramfdk <akramjeet.singh@gmail.com>
2025-09-10 11:49:39 +1000

    Added styles like font for heading and background, added component lifecycle method and fetch data from api instead of hard-coding during component mount

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import CardList from './CardList.jsx';
 import SearchBox from './SearchBox.jsx';
 
 import { Component } from 'react';
-
-
+import './App.css';
 
 class App extends Component {
 
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {
+            this.setState({robots:users});
+        })
     }
 
     onSearchChange = (event) => {
@@ -21,19 +28,26 @@ class App extends Component {
     }
 
     render() {
-        
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
 
-        return (
-            <div className="tc">
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                {/* <CardList robots={this.state.robots} /> */}
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return (
+                <h1>Loading</h1>
+            )
+        }
+        else{
+            const filteredRobots = this.state.robots.filter(robot => {
+                return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            })
+
+            return (
+                <div>
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    {/* <CardList robots={this.state.robots} /> */}
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
